6be455fae0333e666ca1255dbca0284d63749685
MultiRobert <multirobert4444@gmail.com>
2026-01-03 16:28:13 +0900

    Delete assets/js/zoom.js

--- assets/js/zoom.js
+++
@@ -1,9 +0,0 @@
-let scale = 1;
-const system = document.getElementById("solar-system");
-
-window.addEventListener("wheel", e => {
-  e.preventDefault();
-  scale += e.deltaY * -0.001;
-  scale = Math.min(Math.max(0.4, scale), 2);
-  system.style.transform = `scale(${scale})`;
-}, { passive: false });
