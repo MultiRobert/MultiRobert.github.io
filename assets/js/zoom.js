475d31da4319f28fe5d7db7588c6097974239474
MultiRobert <multirobert4444@gmail.com>
2026-01-03 16:21:37 +0900

    Update zoom.js

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,9 +1,9 @@
-const scene = document.getElementById("scene");
 let scale = 1;
+const system = document.getElementById("solar-system");
 
-window.addEventListener("wheel", (e) => {
+window.addEventListener("wheel", e => {
   e.preventDefault();
   scale += e.deltaY * -0.001;
-  scale = Math.min(Math.max(0.4, scale), 2.5);
-  scene.style.transform = `scale(${scale})`;
+  scale = Math.min(Math.max(0.4, scale), 2);
+  system.style.transform = `scale(${scale})`;
 }, { passive: false });
