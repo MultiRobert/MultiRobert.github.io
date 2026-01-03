85f3bdfb1cacb678093a4c82afd616803039ca78
MultiRobert <multirobert4444@gmail.com>
2026-01-03 12:30:33 +0900

    Update space.js

--- assets/js/space.js
+++ assets/js/space.js
@@ -16,3 +16,27 @@ function rotateRight() {
   current = (current + menus.length + 1) % menus.length;
   updateMenu();
 }
+
+const starContainer = document.getElementById("stars");
+
+function createStars(count) {
+  for (let i = 0; i < count; i++) {
+    const star = document.createElement("div");
+
+    const sizeType = Math.random();
+    if (sizeType < 0.6) star.className = "star small";
+    else if (sizeType < 0.9) star.className = "star medium";
+    else star.className = "star large";
+
+    star.style.top = Math.random() * 100 + "%";
+    star.style.left = Math.random() * 100 + "%";
+
+    star.style.animation = `twinkle ${
+      3 + Math.random() * 5
+    }s infinite alternate`;
+
+    starContainer.appendChild(star);
+  }
+}
+
+createStars(180);
