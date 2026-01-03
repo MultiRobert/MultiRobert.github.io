18a17b6da6fedb58e922b3a0081820d389ed7a2f
MultiRobert <multirobert4444@gmail.com>
2026-01-03 13:33:09 +0900

    Create solar.js

---
+++ js/solar.js
@@ -0,0 +1,8 @@
+// Currently purely CSS-driven solar system
+// JS reserved for future interactions:
+// - click planet
+// - zoom
+// - navigation
+// - info panels
+
+console.log("Solar system initialized");
