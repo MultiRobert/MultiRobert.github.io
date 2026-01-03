0adb51a3e83933f95ab1a45f745ac9f39b2e47e2
MultiRobert <multirobert4444@gmail.com>
2026-01-03 12:12:24 +0900

    Create space.js

---
+++ space.js
@@ -0,0 +1,18 @@
+const menus = ["Home", "Projects", "About"];
+let current = 0;
+
+const menuText = document.getElementById("menu-text");
+
+function updateMenu() {
+  menuText.textContent = menus[current];
+}
+
+function rotateLeft() {
+  current = (current - 1 + menus.length) % menus.length;
+  updateMenu();
+}
+
+function rotateRight() {
+  current = (current + 1) % menus.length;
+  updateMenu();
+}
